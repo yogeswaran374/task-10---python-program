3ca75acd6b170cd9a235f37d326f2dd4e58f3054
yogeswaran374 <156685251+yogeswaran374@users.noreply.github.com>
2024-01-30 20:07:34 +0530

    Add files via upload

---
+++ create_a_media_player.py
@@ -0,0 +1,11 @@
+class music_player:
+    def __init__(self, tamil_songs, english_songs):
+        self.tamil = "tamil_songs"
+        self.english = "english_songs"
+        pass
+    def music_playlist(self):
+        return "multiple playlist"
+
+player = music_player(8,8)
+
+print(player.music_playlist())
